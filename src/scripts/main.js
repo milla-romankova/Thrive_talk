65aac77eab08679415fd32c3dfe84ea00a69932c
milla-romankova <milla-romankova@gmail.com>
2020-09-22 00:18:47 +0300

    add slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const btn = document.querySelector('.slider__button');
+
+const slider = document.querySelectorAll('.slider__item');
+
+let i = 0;
+
+btn.addEventListener('click', function() {
+  ++i;
+
+  if (i >= slider.length) {
+    slider[i - 1].classList.remove('slider__item--block');
+    i = 0;
+    slider[i].classList.add('slider__item--block');
+  } else {
+    slider[i - 1].classList.remove('slider__item--block');
+    slider[i].classList.add('slider__item--block');
+  }
+});
